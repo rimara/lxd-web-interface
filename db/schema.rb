4a2e5211ffacfde9eaac6911ae3c461e284c85a4
Taufan Aeroperkasa <taufan@go-jek.com>
2017-08-21 16:40:50 +0700

    Add currently_used migration and change resque path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808145026) do
+ActiveRecord::Schema.define(version: 20170821074622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170808145026) do
     t.string "ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "currently_used"
   end
 
 end
